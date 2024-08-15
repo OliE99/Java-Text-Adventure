e4a3e9cfe45ea1dc7ff7677d4d089a2a2d520700
Oliver Elliott <146334137+OliE99@users.noreply.github.com>
2024-08-15 15:52:19 +0100

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,21 +1,28 @@
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class Game {
 
     JFrame window;
     Container con;
-    JPanel titleNamePanel, startButtonPanel;
-    JLabel titleNameLabel;
+    JPanel titleNamePanel, startButtonPanel, mainTextPanel, choiceButtonPanel, playerPanel;
+    JLabel titleNameLabel, hpLabel, hpLabelNumber, weaponLabel, weaponLabelName;
     Font titleFont = new Font("Times New Roman", Font.PLAIN, 90);
     Font normalFont = new Font("Times New Roman", Font.PLAIN, 30);
-    JButton startButton;
+    JButton startButton, choice1, choice2, choice3, choice4;
+    JTextArea mainTextArea;
+
+    TitleScreenHandler tsHandler = new TitleScreenHandler();
 
     public static void main(String[] args) {
         
@@ -48,6 +55,7 @@ public class Game {
         startButton.setForeground(Color.white);
         startButton.setFont(normalFont);
         startButton.setBorderPainted(false);
+        startButton.addActionListener(tsHandler);
         
         titleNamePanel.add(titleNameLabel);
         startButtonPanel.add(startButton);
@@ -57,5 +65,99 @@ public class Game {
 
         window.setVisible(true);
 
+    }
+
+    public void createGameScreen() {
+
+        // Hide Start Menu
+        titleNamePanel.setVisible(false);
+        startButtonPanel.setVisible(false);
+
+        // Main Text Display
+        mainTextPanel = new JPanel();
+        mainTextPanel.setBounds(100, 100, 600, 250);
+        mainTextPanel.setBackground(Color.black);
+        con.add(mainTextPanel);
+
+        mainTextArea = new JTextArea("Main text area");
+        mainTextArea.setBounds(100, 100, 600, 250);
+        mainTextArea.setBackground(Color.black);
+        mainTextArea.setForeground(Color.white);
+        mainTextArea.setFont(normalFont);
+        mainTextArea.setLineWrap(true);
+        mainTextPanel.add(mainTextArea);
+
+        // Set-up for User Choice Buttons
+        choiceButtonPanel = new JPanel();
+        choiceButtonPanel.setBounds(250, 350, 300, 150);
+        choiceButtonPanel.setBackground(Color.black);
+        choiceButtonPanel.setLayout(new GridLayout(4,1)); // GridLayout changes how content is displayed
+        con.add(choiceButtonPanel);
+
+        choice1 = new JButton("Choice 1");
+        choice1.setBackground(Color.black);
+        choice1.setForeground(Color.white);
+        choice1.setFont(normalFont);
+        choiceButtonPanel.add(choice1);
+        choice1.setBorderPainted(false);
+
+        choice2 = new JButton("Choice 2");
+        choice2.setBackground(Color.black);
+        choice2.setForeground(Color.white);
+        choice2.setFont(normalFont);
+        choiceButtonPanel.add(choice2);
+        choice2.setBorderPainted(false);
+
+        choice3 = new JButton("Choice 3");
+        choice3.setBackground(Color.black);
+        choice3.setForeground(Color.white);
+        choice3.setFont(normalFont);
+        choiceButtonPanel.add(choice3);
+        choice3.setBorderPainted(false);
+
+        choice4 = new JButton("Choice 4");
+        choice4.setBackground(Color.black);
+        choice4.setForeground(Color.white);
+        choice4.setFont(normalFont);
+        choiceButtonPanel.add(choice4);
+        choice4.setBorderPainted(false);
+
+        // Player Info
+        playerPanel = new JPanel();
+        playerPanel.setBounds(100, 15, 600, 50);
+        playerPanel.setBackground(Color.black);
+        playerPanel.setLayout(new GridLayout(1, 4));
+        con.add(playerPanel);
+
+        // HP Info <3
+        hpLabel = new JLabel("HP:");
+        hpLabel.setFont(normalFont);
+        hpLabel.setForeground(Color.white);
+        playerPanel.add(hpLabel);
+        hpLabelNumber = new JLabel();
+        hpLabelNumber.setFont(normalFont);
+        hpLabelNumber.setForeground(Color.white);
+        playerPanel.add(hpLabelNumber);
+
+        // Weapon Info
+        weaponLabel = new JLabel("Weapon:");
+        weaponLabel.setFont(normalFont);
+        weaponLabel.setForeground(Color.white);
+        playerPanel.add(weaponLabel);
+        weaponLabelName = new JLabel();
+        weaponLabelName.setFont(normalFont);
+        weaponLabelName.setForeground(Color.white);
+        playerPanel.add(weaponLabelName);
+
+
+    }
+
+    public class TitleScreenHandler implements ActionListener {
+
+        public void actionPerformed(ActionEvent event) {
+
+            createGameScreen();
+
+        }
     }
 }
